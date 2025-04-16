d6f5a468f97d754648b52e87a0d18b1bfeffadca
Rampatil1305 <ramanagoudsp13@gmail.com>
2025-04-16 11:58:24 +0530

    added java

---
+++ Empo.java
@@ -0,0 +1,24 @@
+
+ public class Empo{
+    int id;
+    String name;
+    int age ;
+    double salary;
+    Empo(int id,String name,int age,double salary){
+        this.id=id;
+        this.name=name;
+        this.age=age;
+        this.double=double;
+
+    }
+    voisplay(){
+      system.out.println ("employee id :"+id);
+      system.out.println ("employee name :"+name);
+      system.out.println ("employee age :"+age);
+      system.out.println ("employee salary :"+salary);
+    }
+    public static void main(String[] args) {
+        Empo obj = new Empo(1, "John Doe", 30, 50000);
+        emp1.display();
+    }
+}
